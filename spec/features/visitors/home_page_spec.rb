1906f04913307edb9872bbe28ef618f373ef3631
Tom Hipkin <tomhipkin@gmail.com>
2019-03-22 15:23:34 +0000

    (feature) Add an explanation message to the front page
    * Turn old service name into a locale variable

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -1,6 +1,7 @@
 feature 'Home page' do
   scenario 'visit the home page' do
     visit root_path
-    expect(page).to have_content 'Purple'
+    expect(page).to have_content(I18n.t('service.name'))
+    expect(page).to have_content(I18n.t('welcome.message'))
   end
 end
