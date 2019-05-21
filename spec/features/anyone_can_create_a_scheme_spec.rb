a39986465958a78ae3b86ee12eadfe674e58866a
Tom Hipkin <tomhipkin@gmail.com>
2019-05-21 14:32:16 +0100

    (refactor) Add a dashboard page title and header
    * This convention that I hope to reuse next will give us a less fragile way to confidently assert what page we are on in the test suite

--- spec/features/anyone_can_create_a_scheme_spec.rb
+++ spec/features/anyone_can_create_a_scheme_spec.rb
@@ -4,6 +4,8 @@ RSpec.feature 'Anyone can create a scheme' do
   scenario 'a scheme can be created' do
     visit root_path
 
+    expect(page).to have_content(I18n.t('page_title.staff.dashboards'))
+
     click_on(I18n.t('scheme.call_to_action'))
 
     expect(page).to have_content(I18n.t('scheme.new.header'))
