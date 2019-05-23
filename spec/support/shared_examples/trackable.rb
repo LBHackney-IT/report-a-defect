e746fea60b6febe509c1d43e37aa6c5559dadc1c
Tom Hipkin <tomhipkin@gmail.com>
2019-05-23 16:45:06 +0100

    (chore) Explicit tests for publicactivity creating update activities too

--- spec/support/shared_examples/trackable.rb
+++ spec/support/shared_examples/trackable.rb
@@ -19,26 +19,38 @@ RSpec.shared_examples 'a trackable resource' do |options|
     it 'creates an activity record with an association to the created resource' do
       resource.save
 
-      result = PublicActivity::Activity.find_by(trackable_id: resource.id, trackable_type: described_class.to_s)
+      result = PublicActivity::Activity.find_by(
+        trackable_id: resource.id, trackable_type: described_class.to_s, key: "#{resource_key_string}.create"
+      )
 
       expect(result).to be_kind_of(PublicActivity::Activity)
       expect(result.trackable).to be_kind_of(described_class)
-    end
-
-    it 'creates an activity record with a key that identifies the event' do
-      resource.save
-
-      result = PublicActivity::Activity.find_by(trackable_id: resource.id, trackable_type: described_class.to_s)
-      expect(result.key).to eq("#{resource_key_string}.create")
-    end
-
-    it 'creates an activity record with the creation date' do
-      resource.save
-
-      result = PublicActivity::Activity.find_by(trackable_id: resource.id, trackable_type: described_class.to_s)
       expect(result.created_at).to eq(Time.zone.now)
     end
   end
 
-  context 'when updated'
+  context "when updating a #{options[:resource]}" do
+    let(:resource_key_string) { options[:resource].to_s.downcase }
+    let(:resource_key_symbol) { resource_key_string.to_sym }
+
+    after(:each) do
+      travel_back
+    end
+
+    it 'creates an activity record with an association to the updated resource' do
+      travel_to Time.zone.parse('2019-05-23')
+      resource = create(resource_key_symbol)
+      travel_to Time.zone.parse('2019-05-24')
+      resource.id = SecureRandom.uuid # update a reliable field
+      resource.save
+
+      result = PublicActivity::Activity.find_by(
+        trackable_id: resource.id, trackable_type: described_class.to_s, key: "#{resource_key_string}.update"
+      )
+
+      expect(result).to be_kind_of(PublicActivity::Activity)
+      expect(result.trackable).to be_kind_of(described_class)
+      expect(result.updated_at).to eq(Time.zone.now)
+    end
+  end
 end
