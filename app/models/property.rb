e6ce9fc39f4944f3a5685486b92fc0264492b05f
Tom Hipkin <tomhipkin@gmail.com>
2019-05-29 13:58:20 +0100

    (chore) Install Postgres for search

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,6 +3,7 @@ class Property < ApplicationRecord
 
   validates :core_name, :address, :postcode, presence: true
 
+  include PgSearch
   include PublicActivity::Model
   tracked owner: ->(controller, _) { controller.current_user if controller }
 end
