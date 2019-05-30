a009705f61ba1ac890b4c56bbe28f50ea9febf48
Tom Hipkin <tomhipkin@gmail.com>
2019-05-30 12:49:39 +0100

    (refactor) Property route moved to multi-line
    * Rubocop marks the line immediately as too long if any edit is attempted

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :estates, controller: 'staff/estates', only: %i[new create show] do
     resources :schemes, controller: 'staff/schemes', only: %i[new create show] do
       resources :priorities, controller: 'staff/priorities', only: %i[new create]
-      resources :properties, controller: 'staff/properties', only: %i[new create edit update]
+      resources :properties, controller: 'staff/properties',
+                             only: %i[new create edit update] do
+      end
     end
   end
   resources :properties, controller: 'staff/properties', only: %i[index show]
