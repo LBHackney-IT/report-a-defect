bbdd31cbd53501b80152156f6bc22e48841aeec2
Tom Hipkin <tomhipkin@gmail.com>
2019-06-03 15:46:11 +0100

    (feature) Defect pages have breadcrumbs

--- spec/features/anyone_can_view_a_defect_spec.rb
+++ spec/features/anyone_can_view_a_defect_spec.rb
@@ -51,20 +51,28 @@ RSpec.feature 'Anyone can view a defect' do
   end
 
   scenario 'can use breadcrumbs to navigate' do
-    # defect = create(:defect)
-    #
-    # visit defects_path(defect)
-    #
-    # within('.govuk-breadcrumbs') do
-    #   expect(page).to have_link('Home', href: '/')
-    #   expect(page).to have_link(
-    #     I18n.t('page_title.staff.estates.show', name: defect.scheme.estate.name),
-    #     href: estate_path(defect.scheme.estate)
-    #   )
-    #   expect(page).to have_link(
-    #     I18n.t('page_title.staff.schemes.show', name: defect.scheme.name),
-    #     href: estate_scheme_path(defect.scheme.estate, defect.scheme)
-    #   )
-    # end
+    defect = create(:defect)
+
+    visit property_defect_path(defect.property, defect)
+
+    within('.govuk-breadcrumbs') do
+      expect(page).to have_link('Home', href: '/')
+
+      expect(page).to have_link(
+        I18n.t('page_title.staff.estates.show', name: defect.property.scheme.estate.name),
+        href: estate_path(defect.property.scheme.estate)
+      )
+      expect(page).to have_link(
+        I18n.t('page_title.staff.schemes.show', name: defect.property.scheme.name),
+        href: estate_scheme_path(defect.property.scheme.estate, defect.property.scheme)
+      )
+      expect(page).to have_link(
+        I18n.t('page_title.staff.properties.show', name: defect.property.address),
+        href: property_path(defect.property)
+      )
+      expect(page).to have_content(
+        I18n.t('page_title.staff.defects.show', reference_number: defect.reference_number)
+      )
+    end
   end
 end
