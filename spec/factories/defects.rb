5c6a1852ed3bb8be13c1aca91225b6369934b77f
Tom Hipkin <tomhipkin@gmail.com>
2019-06-03 15:16:35 +0100

    (fix) Defect factory stores a string instead of an array for description

--- spec/factories/defects.rb
+++ spec/factories/defects.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :defect do
-    description { Faker::Lorem.sentences(2) }
+    description { Faker::Lorem.sentences(2).to_sentence }
     contact_name { Faker::Name.name }
     contact_email_address { Faker::Internet.email }
     contact_phone_number { Faker::Base.numerify('###########') }
