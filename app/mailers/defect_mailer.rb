2d920100be2d8b91488b1433ddec0c45e3296c12
Tom Hipkin <tomhipkin@gmail.com>
2019-06-06 17:15:24 +0100

    (feature) Contractor email includes similar information
    ..to the current process
    * We don't attach defects to users, and we only have one user so we're unable to give the reporting officers name
    * The building type is only property at the moment as we haven't looked at dealing with communal defects

--- app/mailers/defect_mailer.rb
+++ app/mailers/defect_mailer.rb
@@ -1,13 +1,11 @@
 class DefectMailer < ApplicationMailer
   def forward(defect_id)
-    defect = Defect.find(defect_id)
-
-    @defect_reference = defect.reference_number
+    @defect = Defect.find(defect_id)
 
     view_mail(
       NOTIFY_FORWARD_DEFECT_TEMPLATE,
-      to: defect.property.scheme.contractor_email_address,
-      subject: I18n.t('email.defect.forward.subject', reference: defect.reference_number),
+      to: @defect.property.scheme.contractor_email_address,
+      subject: I18n.t('email.defect.forward.subject', reference: @defect.reference_number),
     )
   end
 end
--- spec/mailers/defect_spec.rb
+++ spec/mailers/defect_spec.rb
@@ -15,6 +15,20 @@ RSpec.describe DefectMailer, type: :mailer do
   it 'sends an email to the contractor' do
     expect(mail.subject).to eq(I18n.t('email.defect.forward.subject', reference: defect.reference_number))
     expect(mail.to).to eq([defect.property.scheme.contractor_email_address])
-    expect(body_lines[0]).to match(/# #{I18n.t('app.title')}/)
+    expect(body_lines[0].strip).to match(/# #{I18n.t('app.title')}/)
+    expect(body_lines[2].strip).to match("#{I18n.t('email.defect.forward.headings.title.reference_number')} : #{defect.reference_number}")
+    expect(body_lines[3].strip).to match("#{I18n.t('email.defect.forward.headings.title.created_at')}: #{defect.created_at.to_s(:default)}")
+    expect(body_lines[4].strip).to match("#{I18n.t('email.defect.forward.headings.title.reporting_officer')}: Hackney New Build team")
+    expect(body_lines[6].strip).to match("#{I18n.t('email.defect.forward.headings.title.core_name')}: #{defect.property.core_name}")
+    expect(body_lines[7].strip).to match("#{I18n.t('email.defect.forward.headings.title.address')}: #{defect.property.address}")
+    expect(body_lines[8].strip).to match("#{I18n.t('email.defect.forward.headings.title.location')}: Property")
+    expect(body_lines[10].strip).to match("#{I18n.t('email.defect.forward.headings.title.contact_name')}: #{defect.contact_name}")
+    expect(body_lines[11].strip).to match("#{I18n.t('email.defect.forward.headings.title.contact_phone_number')}: #{defect.contact_phone_number}")
+    expect(body_lines[12].strip).to match("#{I18n.t('email.defect.forward.headings.title.contractor_email_address')}: #{defect.contact_email_address}")
+    expect(body_lines[14].strip).to match("#{I18n.t('email.defect.forward.headings.title.description_of_defect')}: #{defect.description}")
+    expect(body_lines[15].strip).to match("#{I18n.t('email.defect.forward.headings.title.contractor')}: #{defect.property.scheme.contractor_name}")
+    expect(body_lines[16].strip).to match("#{I18n.t('email.defect.forward.headings.title.priority_name')}: #{defect.priority.name}")
+    expect(body_lines[17].strip).to match("#{I18n.t('email.defect.forward.headings.title.target_completion_date')}: #{defect.target_completion_date}")
+
   end
 end
