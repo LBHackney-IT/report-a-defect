235bac10ed52f80d5538149c524bfbc4e54fc15e
Tom Hipkin <tomhipkin@gmail.com>
2019-06-20 11:00:25 +0100

    (feature) Agents can view individual blocks

---
+++ spec/features/anyone_can_view_a_block_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.feature 'Anyone can view a property' do
+  scenario 'a property can be found and viewed' do
+    property = create(:property, address: '1 Hackney Street')
+
+    visit root_path
+
+    expect(page).to have_content(I18n.t('page_title.staff.dashboard'))
+
+    within('form.search') do
+      fill_in 'query', with: 'Hackney'
+      click_on(I18n.t('generic.button.find'))
+    end
+
+    click_on(I18n.t('generic.link.show'))
+
+    expect(page).to have_content(I18n.t('page_title.staff.properties.show', name: property.address))
+
+    within('.scheme_information.scheme_name_and_estate') do
+      expect(page).to have_content(property.scheme.estate.name)
+      expect(page).to have_content(property.scheme.name)
+    end
+
+    within('.scheme_information.scheme_contractor') do
+      expect(page).to have_content(property.scheme.contractor_name)
+      expect(page).to have_content(property.scheme.contractor_email_address)
+    end
+
+    within('.scheme_information.scheme_agent') do
+      expect(page).to have_content(property.scheme.employer_agent_name)
+      expect(page).to have_content(property.scheme.employer_agent_email_address)
+    end
+
+    within('.property_information') do
+      expect(page).to have_content(property.uprn)
+      expect(page).to have_content(property.address)
+      expect(page).to have_content(property.postcode)
+    end
+  end
+
+  scenario 'can use breadcrumbs to navigate' do
+    property = create(:property)
+
+    visit property_path(property)
+
+    within('.govuk-breadcrumbs') do
+      expect(page).to have_link('Home', href: '/')
+      expect(page).to have_link(
+        I18n.t('page_title.staff.estates.show', name: property.scheme.estate.name),
+        href: estate_path(property.scheme.estate)
+      )
+      expect(page).to have_link(
+        I18n.t('page_title.staff.schemes.show', name: property.scheme.name),
+        href: estate_scheme_path(property.scheme.estate, property.scheme)
+      )
+    end
+  end
+end
