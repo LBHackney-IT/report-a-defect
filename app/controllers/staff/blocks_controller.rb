4d356a5f6ea44de7bef3ddcae0b9c682962f7f47
Tom Hipkin <tomhipkin@gmail.com>
2019-06-20 16:22:30 +0100

    (feature) Blocks can be created

--- app/controllers/staff/blocks_controller.rb
+++ app/controllers/staff/blocks_controller.rb
@@ -3,9 +3,38 @@ class Staff::BlocksController < Staff::BaseController
     @block = Block.find(id)
   end
 
+  def new
+    @scheme = Scheme.find(scheme_id)
+    @block = Block.new
+  end
+
+  def create
+    @scheme = Scheme.find(scheme_id)
+    @block = Block.new(block_params)
+    @block.scheme = @scheme
+
+    if @block.valid?
+      @block.save
+      flash[:success] = I18n.t('generic.notice.create.success', resource: 'block')
+      redirect_to estate_scheme_path(@scheme.estate, @scheme)
+    else
+      render :new
+    end
+  end
+
   private
 
   def id
     params[:id]
+  end
+
+  def scheme_id
+    params[:scheme_id]
+  end
+
+  def block_params
+    params.require(:block).permit(
+      :name
+    )
   end
 end
