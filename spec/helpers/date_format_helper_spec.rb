29cf7f4deb98af239d0d16dd16da1720c9512f72
Agz <agz@dxw.com>
2019-06-24 11:36:41 +0100

    (feature) Date time helper for outputting time formats

--- spec/helpers/date_format_helper_spec.rb
+++ spec/helpers/date_format_helper_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe DateHelper, type: :helper do
+RSpec.describe DatetimeHelper, type: :helper do
   describe '#format_date' do
     it 'returns the date in the default format' do
       expect(helper.format_date(Date.new(2017, 10, 7))).to eq '7 October 2017'
+    end
+  end
+
+  describe '#format_time' do
+    it 'returns the time and date in the default format' do
+      expect(helper.format_time(Time.zone.local(2017, 10, 7, 9, 45)))
+        .to eq 'at 09:45am on 7 October 2017'
     end
   end
 end
