f8e7780090caf55de9f4f4f48d7e34758c73e234
Tom Hipkin <tomhipkin@gmail.com>
2019-07-17 16:01:50 +0100

    (fix) Auth0 CLIENT_ID passed via environment variables

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    'Lb0ng5QhbHpaV3f3lQSHPBIer3DSmaOR',
+    ENV['AUTH0_CLIENT_ID'],
     ENV['AUTH0_CLIENT_SECRET'],
     ENV['AUTH0_DOMAIN'],
     callback_path: '/auth/oauth2/callback',
