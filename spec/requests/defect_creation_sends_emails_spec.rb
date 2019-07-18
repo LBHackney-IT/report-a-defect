6a30a1a6b736b1f6bb64898607da53962a118e8b
Tom Hipkin <tomhipkin@gmail.com>
2019-07-18 12:15:32 +0100

    (chore) Readd recently removed request spec for emailing
    * This was removed as part of the Auth0 work https://github.com/LBHackney-IT/report-a-defect/pull/96/commits/277df58e55ea44bd95d769581e92efafe4b42320
    * As described in that commit I chose the option to remove it, now when approaching the feature of making emails optional I believe it has to be in a request spec as driving that work through a feature test doesn't make sense since the user doesn't see anything in the frontend.
    * Added an authentication stub as we can trust the other feature tests to cover the authentication mechanism for creating defects

---
+++ spec/requests/defect_creation_sends_emails_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'Defect creation', type: :request do
+  before(:each) do
+    ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+  end
+
+  after(:each) do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  let(:property) { create(:property) }
+
+  it 'forwards the email to the contractor and employer agent' do
+    allow_any_instance_of(Secured)
+      .to receive(:logged_in_using_omniauth?)
+      .and_return(true)
+
+    defect_attributes = build(:property_defect).attributes
+    defect_attributes.merge!(priority: create(:priority).id)
+    params = {
+      defect: defect_attributes,
+    }
+
+    contractor_message_delivery = instance_double(ActionMailer::MessageDelivery)
+    expect(DefectMailer).to receive(:forward)
+      .with('contractor', property.scheme.contractor_email_address, anything)
+      .and_return(contractor_message_delivery)
+    expect(contractor_message_delivery).to receive(:deliver_later)
+
+    employer_agent_message_delivery = instance_double(ActionMailer::MessageDelivery)
+    expect(DefectMailer).to receive(:forward)
+      .with('employer_agent', property.scheme.employer_agent_email_address, anything)
+      .and_return(employer_agent_message_delivery)
+    expect(employer_agent_message_delivery).to receive(:deliver_later)
+
+    post property_defects_path(property), params: params
+  end
+end
