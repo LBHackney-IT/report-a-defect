9ccb328cfb1f0b063f64056a39e933a762dfc1d8
Tom Hipkin <tomhipkin@gmail.com>
2019-07-18 12:29:52 +0100

    (refactor) Test creation of property and communal area defects
    * Since we are being deliberate about duplicating the property and communal area defect controllers (to allow there similar but slightly different journeys to evolve independently) we should represent this in the request spec too so that both cases are tested.

--- spec/requests/defect_creation_sends_emails_spec.rb
+++ spec/requests/defect_creation_sends_emails_spec.rb
@@ -5,37 +5,69 @@ RSpec.describe 'Defect creation', type: :request do
     ActionMailer::Base.delivery_method = :test
     ActionMailer::Base.perform_deliveries = true
     ActionMailer::Base.deliveries = []
+
+    allow_any_instance_of(Secured)
+      .to receive(:logged_in_using_omniauth?)
+      .and_return(true)
   end
 
   after(:each) do
     ActionMailer::Base.deliveries.clear
   end
 
-  let(:property) { create(:property) }
+  context 'when the defect is for a property' do
+    let(:property) { create(:property) }
 
-  it 'forwards the email to the contractor and employer agent' do
-    allow_any_instance_of(Secured)
-      .to receive(:logged_in_using_omniauth?)
-      .and_return(true)
+    it 'forwards the email to the contractor and employer agent' do
+      defect_attributes = build(:property_defect).attributes
+      defect_attributes.merge!(priority: create(:priority).id)
+      params = {
+        defect: defect_attributes,
+        send_email_to_contractor: 'true',
+        send_email_to_employer_agent: 'true',
+      }
 
-    defect_attributes = build(:property_defect).attributes
-    defect_attributes.merge!(priority: create(:priority).id)
-    params = {
-      defect: defect_attributes,
-    }
+      contractor_message_delivery = instance_double(ActionMailer::MessageDelivery)
+      expect(DefectMailer).to receive(:forward)
+        .with('contractor', property.scheme.contractor_email_address, anything)
+        .and_return(contractor_message_delivery)
+      expect(contractor_message_delivery).to receive(:deliver_later)
 
-    contractor_message_delivery = instance_double(ActionMailer::MessageDelivery)
-    expect(DefectMailer).to receive(:forward)
-      .with('contractor', property.scheme.contractor_email_address, anything)
-      .and_return(contractor_message_delivery)
-    expect(contractor_message_delivery).to receive(:deliver_later)
+      employer_agent_message_delivery = instance_double(ActionMailer::MessageDelivery)
+      expect(DefectMailer).to receive(:forward)
+        .with('employer_agent', property.scheme.employer_agent_email_address, anything)
+        .and_return(employer_agent_message_delivery)
+      expect(employer_agent_message_delivery).to receive(:deliver_later)
 
-    employer_agent_message_delivery = instance_double(ActionMailer::MessageDelivery)
-    expect(DefectMailer).to receive(:forward)
-      .with('employer_agent', property.scheme.employer_agent_email_address, anything)
-      .and_return(employer_agent_message_delivery)
-    expect(employer_agent_message_delivery).to receive(:deliver_later)
+      post property_defects_path(property), params: params
+    end
+  end
 
-    post property_defects_path(property), params: params
+  context 'when the defect is for a communal_area' do
+    let(:communal_area) { create(:communal_area) }
+
+    it 'forwards the email to the contractor and employer agent' do
+      defect_attributes = build(:communal_defect).attributes
+      defect_attributes.merge!(priority: create(:priority).id)
+      params = {
+        defect: defect_attributes,
+        send_email_to_contractor: 'true',
+        send_email_to_employer_agent: 'true',
+      }
+
+      contractor_message_delivery = instance_double(ActionMailer::MessageDelivery)
+      expect(DefectMailer).to receive(:forward)
+        .with('contractor', communal_area.scheme.contractor_email_address, anything)
+        .and_return(contractor_message_delivery)
+      expect(contractor_message_delivery).to receive(:deliver_later)
+
+      employer_agent_message_delivery = instance_double(ActionMailer::MessageDelivery)
+      expect(DefectMailer).to receive(:forward)
+        .with('employer_agent', communal_area.scheme.employer_agent_email_address, anything)
+        .and_return(employer_agent_message_delivery)
+      expect(employer_agent_message_delivery).to receive(:deliver_later)
+
+      post communal_area_defects_path(communal_area), params: params
+    end
   end
 end
