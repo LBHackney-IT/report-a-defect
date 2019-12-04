dd129c912cd611d6d2d10bfa049da228a9c04b95
tosbourn <toby.osbourn@googlemail.com>
2019-12-04 14:59:13 +0000

    Uses a small list for testing
    The sort order isn't important for this test, reducing the list to
    one item is more than enough to prove the test.

--- spec/services/email_due_soon_and_overdue_defects_spec.rb
+++ spec/services/email_due_soon_and_overdue_defects_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EmailDueSoonAndOverdueDefects do
-  let!(:defects) { create_list(:property_defect, 3, status: :outstanding, target_completion_date: 1.day.since) }
+  let!(:defects) { create_list(:property_defect, 1, status: :outstanding, target_completion_date: 1.day.since) }
 
   describe '#call' do
     it 'emails the team' do
