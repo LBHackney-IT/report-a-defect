2e35487abb37ec4c3f35ae786c58513b7d99618d
AtiyyahRawat <atiyyah.rawat@gmail.com>
2025-04-24 13:23:15 +0100

    Force redeployment of ECS service after deploy

--- terraform/development/outputs.tf
+++ terraform/development/outputs.tf
@@ -2,3 +2,13 @@ output "ecr_repository_url" {
   description = "value of the ECR repository URL"
   value       = aws_ecr_repository.app_repository.repository_url
 }
+
+output "cluster_name" {
+  description = "value of the ECS cluster name"
+  value       = aws_ecs_cluster.app_cluster.name
+}
+
+output "service_name" {
+  description = "value of the ECS service name"
+  value       = aws_ecs_service.app_service.name
+}
