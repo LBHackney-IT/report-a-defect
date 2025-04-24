0ee0e696e66d2ea17ccda00cb09d2fc4e745234f
AtiyyahRawat <atiyyah.rawat@gmail.com>
2025-04-24 01:27:29 +0100

    Add second ecr repo

--- terraform/development/outputs.tf
+++ terraform/development/outputs.tf
@@ -3,3 +3,8 @@ output "ecr_repository_url" {
   description = "value of the ECR repository URL"
   value       = module.aws-ecs-lbh.ecr_repository_url
 }
+
+output "ecr_repo_url_2" {
+  description = "value of the ECR repository URL"
+  value       = aws_ecr_repository.app_repository.repository_url
+}
