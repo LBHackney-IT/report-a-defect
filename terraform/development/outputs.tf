6666ab25e3b21c2c8567ede35a114f82d4305ba8
AtiyyahRawat <atiyyah.rawat@gmail.com>
2025-04-25 21:22:06 +0100

    Migrate DB in pipeline
    - Updated terraform-init command to initialize and apply Terraform in one step.
    - Added new outputs for task definition name, subnet IDs, and ECS security group IDs in Terraform.
    - Implemented a new job for database migration using AWS ECS run-task command.

--- terraform/development/outputs.tf
+++ terraform/development/outputs.tf
@@ -12,3 +12,18 @@ output "service_name" {
   description = "value of the ECS service name"
   value       = aws_ecs_service.app_service.name
 }
+
+output "task_definition_name" {
+  description = "value of the ECS task definition name"
+  value       = aws_ecs_task_definition.app_task.family
+}
+
+output "subnet_ids" {
+  description = "value of the subnet IDs"
+  value       = data.aws_subnets.private_subnets.ids
+}
+
+output "ecs_security_group_ids" {
+  description = "value of the ECS security group IDs"
+  value       = aws_security_group.ecs_task_sg.id
+}
