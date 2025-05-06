781049d832c7792c9e2933615fa9d52293ac43fc
AtiyyahRawat <atiyyah.rawat@gmail.com>
2025-05-06 18:08:10 +0100

    Remove unnecessary logging from Auth0 callback and ensure redirect uses named route

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,8 +1,5 @@
 class Auth0Controller < ApplicationController
   def callback
-    Rails.logger.info "Session before assignment: #{request.session.inspect}"
-    Rails.logger.info "Host Header: #{request.headers['Host']}"
-
     # This stores all the user information that came from Auth0
     # and the IdP
     session[:userinfo] = request.env['omniauth.auth']
@@ -10,7 +7,7 @@ class Auth0Controller < ApplicationController
     Rails.logger.info "Session after assignment: #{request.session.inspect}"
 
     # Redirect to the URL you want after successful auth
-    redirect_to '/dashboard'
+    redirect_to dashboard_path
   end
 
   def failure
