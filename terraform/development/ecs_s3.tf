b629a5f290c1a9bf789a789011bb00af705f0210
AtiyyahRawat <atiyyah.rawat@gmail.com>
2025-05-12 17:00:24 +0100

    Add s3:DeleteObject permission to ECS task policy

--- terraform/development/ecs_s3.tf
+++ terraform/development/ecs_s3.tf
@@ -155,6 +155,7 @@ resource "aws_iam_policy" "ecs_task_policy" {
         Action : [
           "s3:GetObject",
           "s3:PutObject",
+          "s3:DeleteObject",
         ],
         Resource : [
           aws_s3_bucket.image_bucket.arn,
