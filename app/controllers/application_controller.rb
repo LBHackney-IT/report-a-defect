d5aa61564d32a3796c7e928062a1b4704bd25f11
AtiyyahRawat <atiyyah.rawat@gmail.com>
2025-05-12 15:47:00 +0100

    Comment out protect_from_forgery in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Logout
   include PublicActivity::StoreController
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_action :check_staging_auth, except: :check
 
