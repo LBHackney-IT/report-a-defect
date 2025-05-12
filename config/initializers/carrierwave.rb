71c82b9d6e91279996bbbb900eb95f801d035ce9
AtiyyahRawat <atiyyah.rawat@gmail.com>
2025-05-12 15:59:44 +0100

    Reset AWS access keys to empty strings in CarrierWave configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: "dummy_access_key_id", # Credentials are set with IAM profile but this is required for fog
-      aws_secret_access_key: "dummy_secret_access_key",
+      aws_access_key_id: "", # Credentials are set with IAM profile but this is required for fog
+      aws_secret_access_key: "",
       region: ENV['AWS_REGION'],
       use_iam_profile: true,
     }
